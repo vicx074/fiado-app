ef2985cf6c83995e3b3af833abfc7c4b158aed41
vicx074 <victororg22@gmail.com>
2025-06-21 19:38:55 -0300

    Definindo rotas de api env

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.REACT_APP_API_URL,
 });
-
 // Adiciona o token JWT automaticamente em todas as requisições, se existir
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
